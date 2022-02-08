5ce2ff9bcbfef244562eb67dcec4176956e9d05b
jonathan <jonathangomezit@gmail.com>
2022-02-08 17:40:32 -0300

    added script mandril or mailchimp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Layout } from 'components/Layout'
 import { HeadMetas } from 'components/HeadMetas'
 import { StandByScreen } from '../components/StandByScreen/index';
 import { useEffect, useState } from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
@@ -25,6 +26,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script
+        id="mcjs"
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c2ea63dc1e80b4c44f9b6fcbc/0efc99f328e393076df91aaf9.js");`
+        }}
+      />
       </>)
 }
 
