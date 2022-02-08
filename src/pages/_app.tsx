d226041b0d3816dd36ca1d2f2d39995b5bcf09d2
jonathan <jonathangomezit@gmail.com>
2022-02-08 18:35:09 -0300

    bye bye script of mandril

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,34 +5,14 @@ import { Layout } from 'components/Layout'
 import { HeadMetas } from 'components/HeadMetas'
 import { StandByScreen } from '../components/StandByScreen/index';
 import { useEffect, useState } from 'react';
-import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const [isShow, setIsShow] = useState<boolean>(true);
-
-  // useEffect(() => {
-
-  //   setTimeout(() => {
-
-  //     setIsShow(false)
-
-  //   }, 3000)
-  // },[])
-
   return (<>
       <HeadMetas title={pageProps.title}/>
-      {/* {isShow && <StandByScreen/>} */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      <Script
-        id="mcjs"
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c2ea63dc1e80b4c44f9b6fcbc/0efc99f328e393076df91aaf9.js");`
-        }}
-      />
       </>)
 }
 
